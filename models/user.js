1df6ff193b24b6132004007a15c3270a9d73ee1f
cecl <cecile.porchet@heig-vd.ch>
2018-02-23 15:11:37 +0100

    Messages d'erreur - création user

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 //Define the User Schema
 const userSchema = new Schema({
-  firstName: {type: String, minlength: 2, maxlength: 20},
-  lastName: {type: String, minlength: 2, maxlength: 20},
+  firstName: {type: String, minlength: [2, 'Too few letters'], maxlength: [20, 'Too many letters']},
+  lastName: {type: String, minlength: [2, 'Too few letters'], maxlength: [20, 'Too many letters']},
   role: {type: String, enum:['citizen','manager'], default: 'citizen'},
   createdAt: {type: Date, default: Date.now}
 });
