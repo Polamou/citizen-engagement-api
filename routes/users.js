0efa1f958f0fb1f1cbe211b9b9ec6890877e988f
Nicolas Mermoud <nicolas.mermoud@heig-vd.ch>
2018-02-23 15:07:27 +0100

    Add base for users model

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* ADD new user */
+router.post('/', function(req, res, next) {
+  res.send('post user');
+});
+
+/* GET users listing */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('GET users listing');
+});
+
+/* GET user by id */
+router.get('/:id', function(req, res, next) {
+  res.send('GET user by id');
+});
+
+/* PUT user by id */
+router.put('/:id', function(req, res, next) {
+  res.send('PUT user by id');
+});
+
+/* DELETE user by id */
+router.delete('/:id', function(req, res, next) {
+  res.send('DELETE user by id');
 });
 
 module.exports = router;
