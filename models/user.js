0abbe06378318ede8a339c2b03924e005358775e
Timothée Delapierre <timothee.delapierre@gmail.com>
2018-02-23 14:51:29 +0100

    Typo fixed on users.js

--- models/user.js
+++ models/user.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   firstName: {type: String, min: 2, max: 20},
   lastName: {type: String, min: 2, max: 20},
-  role: {type: String, enum:['citizen','manager'],default: 'citizen'},
+  role: {type: String, enum:['citizen','manager'], default: 'citizen'},
   createdAt: {type: Date, default: Date.now}
 });
 
-mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
