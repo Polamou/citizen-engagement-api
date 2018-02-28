49f6447677a2d18090102458ec31b52275280860
Nicolas Mermoud <nicolas.mermoud@heig-vd.ch>
2018-02-28 14:08:21 +0100

    Change coordonnees to geolocation

--- models/issue.js
+++ models/issue.js
@@ -8,7 +8,7 @@ const issueSchema = new Schema({
   status: {type: String, enum:['new','inProgress', 'canceled', 'completed'], default: 'new'},
   description: {type: String, maxlength: [1000, 'Too many letters'], required: false},
   imageUrl: {type: String, maxlength: [500, 'Too many letters'], required: false},
-  coordonnees: {type: mongoose.Schema.Types.Point, required: true},
+  geolocation: {type: mongoose.Schema.Types.Point, required: true},
   tags: [{ type: String, maxlength: 50}],
   createdAt: {type: Date, default: Date.now},
   updatedAt: {type: Date},
