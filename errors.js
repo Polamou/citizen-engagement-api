279101a28f606c10f2c3ba43ec8dfe6285fa3743
Timothée Delapierre <timothee.delapierre@gmail.com>
2018-03-01 10:31:01 +0100

    Able to throw 422 error

--- errors.js
+++ errors.js
@@ -4,3 +4,8 @@ exports.notFound = function (message){
   err.status = 404;
   return err;
 }
+exports.unprocessableError = function (message){
+  let err = new Error(message);
+  err.status = 422;
+  return err;
+}
