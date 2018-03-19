617e5333603d36291c4ba04c3df7f03732f1ab77
Timothée Delapierre <timothee.delapierre@gmail.com>
2018-03-19 13:22:18 +0100

    Corrected the required values

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 //Define the User Schema
 const userSchema = new Schema({
-  firstName: {type: String, minlength: [2, 'Too few letters'], maxlength: [20, 'Too many letters']},
-  lastName: {type: String, minlength: [2, 'Too few letters'], maxlength: [20, 'Too many letters']},
+  firstName: {type: String, minlength: [2, 'Too few letters'], maxlength: [20, 'Too many letters'], required: true},
+  lastName: {type: String, minlength: [2, 'Too few letters'], maxlength: [20, 'Too many letters'], required: true},
   role: {type: String, enum:['citizen','manager'], default: 'citizen'}
 },
 {
